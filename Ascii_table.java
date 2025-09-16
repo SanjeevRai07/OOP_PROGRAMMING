b7f9c70e1b6cfa0cb927aa4f8e080ec4140ca9ed
Sanjeev Rai <146112569+SanjeevRai07@users.noreply.github.com>
2025-09-16 11:30:18 +0530

    Add files via upload

---
+++ Ascii_table.java
@@ -0,0 +1,8 @@
+public class Ascii_table {
+    public static void main(String[] args) {
+        // Displaying ASCII values of characters from A to Z
+        char Devil= 'A';
+        int ASCII_Value =Devil;
+        System.out.println("The Ascii value is :"  + Devil + " is " + ASCII_Value);
+    }
+}
