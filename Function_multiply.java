f09b2c4abe027439db6400d37486e2cd4163be6e
Sanjeev Rai <146112569+SanjeevRai07@users.noreply.github.com>
2025-09-25 10:08:21 +0530

    Add files via upload

---
+++ Function_multiply.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Function_multiply{
+    public static int Calculate_Product(int a ,int b){
+        return a*b;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Product of two number is:" + Calculate_Product(a,b));
+    }
+
+}
+    
+
